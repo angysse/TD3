abe0fa7a6b749b6e3a7bfe0fb6069afcaf85c07e
angysse <lebbolo@et.esiea.fr>
2020-04-20 21:02:41 +0200

    Ajout de l'appel serveur

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -5,11 +5,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co/";
+
 
 
     private RecyclerView recyclerView;
@@ -20,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       showList();
+       makeApiCall();
+    }
+    private void showList() {
+
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
@@ -36,6 +55,44 @@ public class MainActivity extends AppCompatActivity {
         }// define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+
+    }
+
+
+    private void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        PokeApi PokeApi = retrofit.create(PokeApi.class);
+
+        Call<RestPokemonResponse> call = PokeApi.getPokemonResponse();
+       call.enqueue(new Callback<RestPokemonResponse>() {
+           @Override
+           public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+               if (response.isSuccessful() && response.body() != null){
+                   List<Pokemon> pokemonList = response.body().getResults();
+                   Toast.makeText(getApplicationContext(), "API Succes", Toast.LENGTH_SHORT).show();
+               } else {
+                   showError();
+               }
+
+           }
+
+           @Override
+           public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                showError();
+           }
+       });
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
 
