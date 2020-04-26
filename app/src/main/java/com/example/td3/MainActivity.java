00a0bcc4422921154187ff9ff6e821a44f46fe88
angysse <lebbolo@et.esiea.fr>
2020-04-26 14:28:41 +0200

    Affichage des données reçues du serveur

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -34,26 +34,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       showList();
+
        makeApiCall();
     }
-    private void showList() {
-
-
+    private void showList(List<Pokemon>pokemonList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        // use this setting to
-        // improve performance if you know that changes
-        // in content do not change the layout size
-        // of the RecyclerView
         recyclerView.setHasFixedSize(true);
-        // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }// define an adapter
-        mAdapter = new ListAdapter(input);
+
+        mAdapter = new ListAdapter(pokemonList);
         recyclerView.setAdapter(mAdapter);
 
     }
@@ -77,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
                if (response.isSuccessful() && response.body() != null){
                    List<Pokemon> pokemonList = response.body().getResults();
-                   Toast.makeText(getApplicationContext(), "API Succes", Toast.LENGTH_SHORT).show();
+                   showList(pokemonList);
                } else {
                    showError();
                }
